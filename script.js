df0dbdf22eed11f17cf43ad48c520676a7f20e94
vishnu172943 <vishnureddy2048@gmail.com>
2024-03-04 01:07:43 +0530

    update

---
+++ script.js
@@ -0,0 +1,51 @@
+//Implement all your function here to make it a working application.
+const genres=document.getElementById('genres');
+const movieInfo=document.getElementById('movieInfo');
+const nextBtn=document.getElementById('likebtn');
+genres.addEventListener('change',()=>{
+    let genID=genres.value;
+    const request = new XMLHttpRequest();
+    const YOUR_API_KEY = "2cf374672dbb2bdcdf9d6a91178b89d1";
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${YOUR_API_KEY}&with_genres=${genID}&page=1&sort_by=popularity.desc
+    `;
+     let id=0;
+    request.open('GET', url); 
+    request.send();
+    request.addEventListener('load', function() {
+        if (this.status === 200) {
+            const data = JSON.parse(this.responseText);
+            console.log(data); 
+              let imageBaseUrl = "https://image.tmdb.org/t/p/";
+    let imageSize = "w300"; 
+    let posterPath = data.results[id].poster_path; // Extract this from the API response// Extract this from the API response
+    let fullImageUrl = imageBaseUrl + imageSize + posterPath; 
+    
+    
+    let movieIn=` <div id="moviePoster"><img src="${fullImageUrl}" alt="hello"></div>
+    <div id="movieText"><h1>${data.results[id].overview}</h1></div>
+    `
+    movieInfo.innerHTML=movieIn;
+     
+    nextBtn.addEventListener('click',()=>{
+        const posterPath = data.results[id++].poster_path; // Extract this from the API response
+        const fullImageUrl = imageBaseUrl + imageSize + posterPath; 
+    
+    
+        const movieIn=` <div id="moviePoster"><img src="${fullImageUrl}" alt="hello"></div>
+        <div id="movieText"><h1>${data.results[id++].overview}</h1></div>
+    `
+      movieInfo.innerHTML=movieIn;
+    
+    })
+    
+         } else {
+            console.error("Error fetching data:", this.status);
+        }
+     });
+    
+    
+     
+    
+     
+ })
+ 
